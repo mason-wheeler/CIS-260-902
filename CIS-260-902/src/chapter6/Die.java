6c793d1efcee621646364fab28f6c4d75be55761
Mason Wheeler <mwheeler050@gmail.com>
2019-11-20 20:53:40 -0600

    Finished 14

---
+++ CIS-260-902/src/chapter6/Die.java
@@ -0,0 +1,55 @@
+//Mason Wheeler Section 902
+
+package chapter6;
+
+import java.util.Random;
+
+public class Die 
+{
+	private int sides;		//Number of sides
+	private int value;		//The die's value
+	
+	/**
+	 	The constructor performs an initial roll of the die.
+	 	@param	numSides The number of sides for this die.
+	 */
+	
+	public Die(int numSides)
+	{
+		sides = numSides;
+		roll();
+	}
+
+	/**
+	 	The roll method simulates the rolling of the die.
+	 */
+	
+	public void roll() 
+	{
+		//Create a Random Object.
+		Random rand = new Random();
+		
+		//Get a random value for the die.
+		value = rand.nextInt(sides) + 1;
+	}
+	
+	/**
+	 	getSides method
+	 	@return The number of sides for this die. 
+	 */
+	
+	public int getSides()
+	{
+		return sides;
+	}
+	
+	/**
+	 	getValue method
+	 	@return The value of the die.
+	 */
+	
+	public int getValue()
+	{
+		return value;
+	}
+}//end of class
